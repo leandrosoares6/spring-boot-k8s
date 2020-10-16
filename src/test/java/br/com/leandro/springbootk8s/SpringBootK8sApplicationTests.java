89f9c654cc81aae6b558da90474f4712e0af55da
Leandro Neves <leandrosoares6@hotmail.com>
2020-10-16 15:40:54 -0300

    First commit

---
+++ src/test/java/br/com/leandro/springbootk8s/SpringBootK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leandro.springbootk8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
